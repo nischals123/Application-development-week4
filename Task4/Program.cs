8ed6dc6692328d186f883cff765dc2249872bbf9
Nischal <nischals054@gmail.com>
2025-11-18 16:04:25 +0545

    Updated

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,5 +4,5 @@ using Task4;
 Player p1 = new Player();
 Console.WriteLine("Player 1 -> Name: " + p1.playerName + ", Level: " + p1.level + ", Health: " + p1.health);
 
-Player p2 = new Player("Debina", 7, 120);
+Player p2 = new Player("Nischal", 7, 130);
 Console.WriteLine("Player 2 -> Name: " + p2.playerName + ", Level: " + p2.level + ", Health: " + p2.health);
--- Task4/obj/Debug/net9.0/Task4.AssemblyInfo.cs
+++ Task4/obj/Debug/net9.0/Task4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792927453e5cac771c45039636c5173c9dd87d1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4cdc5d4ec3c4a2ffecb768c8fd03ddd33048997")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
