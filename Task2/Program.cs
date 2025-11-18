4da6a544929183f34af168d542ec3c31e9d8e395
Nischal <nischals054@gmail.com>
2025-11-18 11:45:29 +0545

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+        calc.PrintWelcome();
+        int sum = calc.Add(5, 7);
+        Console.WriteLine("Addition: " + sum);
+        int product2 = calc.Multiply(10);
+        Console.WriteLine("Multiplication (10 * default 1): " + product2);
+    }
+}
